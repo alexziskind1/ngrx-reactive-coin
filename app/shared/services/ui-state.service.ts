43614b7881c5d13dc0a05b6f7088f6160cb34e11
Alexander Ziskind <alex@nuvious.com>
2017-07-18 15:31:42 -0400

    update to azure func call

--- app/shared/services/ui-state.service.ts
+++ app/shared/services/ui-state.service.ts
@@ -6,11 +6,20 @@ import { Coin } from '../../shared/models/coin.interface';
 @Injectable()
 export class UIStateService {
 
-    private baseUrl: string = 'https://api.cryptonator.com/api';
+    //private baseUrl: string = 'https://api.cryptonator.com/api';
+    private baseUrl: string = 'https://tns-fn-app1.azurewebsites.net/api';
+
     constructor(private http: Http) { }
 
     getCoinPrice(currencyCode: string): Observable<Coin> {
+        /*
         return this.http.get(`${this.baseUrl}/ticker/${currencyCode}-usd`)
             .map(res => res.json());
+            */
+        return this.http.get(`${this.baseUrl}/HttpTriggerJS1?code=${currencyCode}`)
+            .map(res => {
+                return res.json();
+            });
+
     }
 }
